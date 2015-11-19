2a90741aa2cf6afa11e587afce3d5161d96b766f
netroby <netroby@users.noreply.github.com>
2015-11-19 09:05:13 +0800

    Update hackfonts.sh

--- hackfonts.sh
+++ hackfonts.sh
@@ -4,8 +4,8 @@ if [ ! -f /usr/bin/unzip ]; then
     echo "Please installed unzip first"
     exit 0
 fi
-file=Hack-v2_015-ttf.zip
-wget https://github.com/chrissimpkins/Hack/releases/download/v2.015/$file
+file=Hack-v2_018-ttf.zip
+wget https://github.com/chrissimpkins/Hack/releases/download/v2.018/$file
 unzip $file
 sudo chmod a+x Hack-*.ttf
 sudo mv Hack-*.ttf /usr/share/fonts/truetype/
